e8a25dd40a85e1c2325d9c02e8009748e2b34d3c
Clément <clement@numericite.eu>
2023-03-09 15:53:14 +0100

    fix: eslint prettier config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,24 +6,24 @@ import PublicLayout from '@/layouts/PublicLayout';
 
 // Only in TypeScript projects
 declare module '@codegouvfr/react-dsfr/next-pagesdir' {
-  interface RegisterLink {
-    Link: typeof Link;
-  }
+	interface RegisterLink {
+		Link: typeof Link;
+	}
 }
 
 const { withDsfr, dsfrDocumentApi } = createNextDsfrIntegrationApi({
-  defaultColorScheme: 'system',
-  Link
+	defaultColorScheme: 'system',
+	Link
 });
 
 export { dsfrDocumentApi };
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <PublicLayout>
-      <Component {...pageProps} />
-    </PublicLayout>
-  );
+	return (
+		<PublicLayout>
+			<Component {...pageProps} />
+		</PublicLayout>
+	);
 }
 
 export default withDsfr(App);
