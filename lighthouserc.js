d6eb2c89ff300ca6d385cfbbb1db813ab095ed6a
Clément <clement@numericite.eu>
2023-03-09 10:41:33 +0100

    fix: lightouserc config for NextJS

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,20 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './'
+      startServerCommand: "npm run start",
+      startServerReadyPattern: "ready on",
+      url: ["http://localhost:3000"],
+      numberOfRuns: 1,
+      settings: {
+        preset: "desktop",
+      },
+    },
+    assert: {
+      preset: "lighthouse:recommended",
     },
     upload: {
-      target: 'temporary-public-storage'
-    }
-  }
+      target: "temporary-public-storage",
+    },
+    server: {},
+  },
 };
