7cf3547eb15a90c3655659eb07dca7ac05b351f2
Clément <clement@numericite.eu>
2023-03-10 16:26:58 +0100

    fix: remove next-transpile-modules & redesign jest config

--- jest.config.js
+++ jest.config.js
@@ -6,17 +6,12 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-	moduleDirectories: ['node_modules', '<rootDir>/'],
+	moduleNameMapper: {
+		'^@/components/(.*)$': '<rootDir>/components/$1',
+
+		'^@/pages/(.*)$': '<rootDir>/pages/$1'
+	},
 	testEnvironment: 'jest-environment-jsdom'
 };
 
-async function jestConfig() {
-	const nextJestConfig = await createJestConfig(customJestConfig)();
-	nextJestConfig.transformIgnorePatterns[0] =
-		'/node_modules/(?!(@codegouvfr))/';
-	return nextJestConfig;
-}
-
-module.exports = jestConfig;
-
-// module.exports = createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
-const withTM = require('next-transpile-modules')(['@codegouvfr/react-dsfr']);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withTM({
+const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	i18n: {
-		locales: ['fr'],
-		defaultLocale: 'fr'
-	},
 	webpack: config => {
 		config.module.rules.push({
 			test: /\.woff2$/,
 			type: 'asset/resource'
 		});
-
 		return config;
-	}
-});
+	},
+	//This option requires Next 13.1 or newer, if you can't update you can use this plugin instead: https://github.com/martpie/next-transpile-modules
+	transpilePackages: ['@codegouvfr/react-dsfr']
+};
 
 module.exports = nextConfig;
