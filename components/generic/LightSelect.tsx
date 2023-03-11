6d43854c6956fd1afe530f50d1cfebecd05a9353
Clément <clement@numericite.eu>
2023-03-11 13:28:45 +0100

    feat: add edition selector component as generic

---
+++ components/generic/LightSelect.tsx
@@ -0,0 +1,60 @@
+import { makeStyles } from '@codegouvfr/react-dsfr/tss';
+import { useEffect, useState } from 'react';
+import { Select } from '@codegouvfr/react-dsfr/Select';
+import { fr } from '@codegouvfr/react-dsfr';
+
+type Props = {
+	options: { label: string; value: string | number }[];
+	defaultValue?: string | number;
+	onChange?(value: string | number): void;
+	placeholder?: string;
+};
+
+export function LightSelect(props: Props) {
+	const { onChange, options, placeholder, defaultValue } = props;
+
+	const { classes, cx } = useStyles();
+
+	const [value, setValue] = useState<string | number>(
+		defaultValue ? defaultValue : ''
+	);
+
+	useEffect(() => {
+		if (value !== undefined && !!onChange) onChange(value);
+	}, [value]);
+
+	return (
+		<Select
+			label=""
+			className={cx(classes.root)}
+			nativeSelectProps={{
+				onChange: event => setValue(event.target.value),
+				value
+			}}
+		>
+			{placeholder && (
+				<option value="" disabled hidden>
+					{placeholder}
+				</option>
+			)}
+			{options.map(opt => (
+				<option value={opt.value}>{opt.label}</option>
+			))}
+		</Select>
+	);
+}
+
+const useStyles = makeStyles()(theme => ({
+	root: {
+		['select']: {
+			width: 'auto',
+			backgroundColor: 'transparent',
+			color: theme.decisions.background.actionHigh.blueFrance.default,
+			fontWeight: 'bold',
+			boxShadow: 'none',
+			...fr.typography[20].style,
+			padding: fr.spacing('2v'),
+			paddingRight: fr.spacing('10v')
+		}
+	}
+}));
--- components/top250/Header.tsx
+++ components/top250/Header.tsx
@@ -2,6 +2,7 @@ import { makeStyles } from '@codegouvfr/react-dsfr/tss';
 import { fr } from '@codegouvfr/react-dsfr';
 import { ReactNode } from 'react';
 import { SearchBar } from '@codegouvfr/react-dsfr/SearchBar';
+import { LightSelect } from '../generic/LightSelect';
 
 type Props = {
 	title: ReactNode;
@@ -16,6 +17,14 @@ export function Top250Header(props: Props) {
 	return (
 		<div className={cx(classes.root)}>
 			<h1 className={cx(classes.title)}>{title}</h1>
+			<LightSelect
+				options={[
+					{ label: 'Avril 2023', value: '04-2023' },
+					{ label: 'Octobre 2022', value: '10-2022' },
+					{ label: 'Juillet 2022', value: '07-2022' }
+				]}
+				defaultValue={'04-2023'}
+			/>
 			<SearchBar className={cx(classes.search)} label={searchLabel} />
 		</div>
 	);
@@ -27,15 +36,18 @@ const useStyles = makeStyles()(theme => ({
 		paddingBottom: fr.spacing('18v')
 	},
 	title: {
-		fontSize: '3rem',
-		lineHeight: '3.625rem',
-		color: theme.decisions.background.actionHigh.blueFrance.default
+		...fr.typography[11].style,
+		color: theme.decisions.background.actionHigh.blueFrance.default,
+		marginBottom: fr.spacing('3v')
 	},
 	search: {
 		width: '50%',
 		marginTop: fr.spacing('11v'),
-		['input']: {
-			backgroundColor: theme.decisions.background.alt.blueFrance.default
+		['input.fr-input']: {
+			backgroundColor: theme.decisions.background.alt.blueFrance.default,
+			['::placeholder, ::-ms-input-placeholder']: {
+				color: theme.decisions.background.actionHigh.blueFrance.default
+			}
 		}
 	}
 }));
