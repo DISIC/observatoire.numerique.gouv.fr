d120869fb0f81808c04a9e27369d2c0133f951ae
Clément <clement@numericite.eu>
2023-03-16 16:58:28 +0100

    feat: add modal integration & hover to ColumnHeader

--- components/table/ColumnHeaderDefinition.tsx
+++ components/table/ColumnHeaderDefinition.tsx
@@ -3,25 +3,51 @@ import {
 	type RiIconClassName,
 	fr
 } from '@codegouvfr/react-dsfr';
+import { createModal } from '@codegouvfr/react-dsfr/Modal';
 import { ReactNode } from 'react';
 import { makeStyles } from '@codegouvfr/react-dsfr/tss';
+import { Button } from '@codegouvfr/react-dsfr/Button';
+import { string } from 'zod';
 
 type Props = {
 	icon: FrIconClassName | RiIconClassName;
 	text: ReactNode;
+	infos: {
+		title: string;
+		content: ReactNode;
+	};
 };
 
 export function ColumnHeaderDefinition(props: Props) {
-	const { icon, text } = props;
+	const { icon, text, infos } = props;
 	const { classes, cx } = useStyles();
 
+	const { InfosModal, infosModalButtonProps } = createModal({
+		name: 'infos',
+		isOpenedByDefault: false
+	});
+
 	return (
-		<div className={cx(classes.root)}>
-			<i className={fr.cx(icon)} />
-			<span role="text" className={cx(classes.text)}>
-				{text}
-			</span>
-		</div>
+		<>
+			<Button className={cx(classes.root)} {...infosModalButtonProps}>
+				<i className={fr.cx(icon)} />
+				<span role="text" className={cx(classes.text)}>
+					{text}{' '}
+					<i className={cx(fr.cx('ri-information-line'), classes.icon)} />
+				</span>
+			</Button>
+			<InfosModal
+				title={infos.title}
+				buttons={[
+					{
+						onClick: () => console.log('en savoir plus clicked'),
+						children: 'En savoir plus'
+					}
+				]}
+			>
+				{infos.content}
+			</InfosModal>
+		</>
 	);
 }
 
@@ -34,9 +60,21 @@ const useStyles = makeStyles()(theme => ({
 		padding: fr.spacing('4v'),
 		color: theme.decisions.text.actionHigh.blueFrance.default,
 		fontWeight: 'bold',
-		textAlign: 'center'
+		textAlign: 'center',
+		cursor: 'pointer',
+		borderRadius: '8px',
+		backgroundColor: 'transparent',
+		[':hover']: {
+			backgroundColor:
+				theme.decisions.background.alt.blueFrance.default + ' !important'
+		}
 	},
 	text: {
 		marginTop: fr.spacing('3v')
+	},
+	icon: {
+		['&::before']: {
+			'--icon-size': fr.typography[18].style.fontSize
+		}
 	}
 }));
