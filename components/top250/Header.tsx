d8791ce4e2667f71f514af1c4ff3e83530cffc63
Clément <clement@numericite.eu>
2023-03-31 23:37:54 +0200

    fix: use last edition name in dropdown

--- components/top250/Header.tsx
+++ components/top250/Header.tsx
@@ -3,89 +3,94 @@ import { fr } from '@codegouvfr/react-dsfr';
 import { ReactNode, useState } from 'react';
 import { SearchBar } from '@codegouvfr/react-dsfr/SearchBar';
 import { LightSelect } from '../generic/LightSelect';
-
+import { useEditions } from '@/utils/api';
 type Props = {
 	title: ReactNode;
 	searchLabel: string;
 	onSearch: (value: string) => void;
 };
 
-const editionOptions = [
-	{ label: 'Avril 2023', value: '04-2023' },
-	{
-		label: 'Octobre 2022',
-		value: '10-2022',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-octobre/'
-	},
-	{
-		label: 'Juillet 2022',
-		value: '07-2022',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-juillet/'
-	},
-	{
-		label: 'Avril 2022',
-		value: '04-2022',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-avril/'
-	},
-	{
-		label: 'Janvier 2022',
-		value: '01-2022',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-janvier/'
-	},
-	{
-		label: 'Octobre 2021',
-		value: '10-2021',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-octobre/'
-	},
-	{
-		label: 'Juillet 2021',
-		value: '07-2021',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-juillet/'
-	},
-	{
-		label: 'Avril 2021',
-		value: '04-2021',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-avril/'
-	},
-	{
-		label: 'Janvier 2021',
-		value: '01-2021',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-janvier/'
-	},
-	{
-		label: 'Octobre 2020',
-		value: '10-2020',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-octobre/'
-	},
-	{
-		label: 'Juillet 2020',
-		value: '07-2020',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-juillet/'
-	},
-	{
-		label: 'Avril 2020',
-		value: '04-2020',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-avril/'
-	},
-	{
-		label: 'Janvier 2020',
-		value: '01-2020',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-janvier/'
-	},
-	{
-		label: 'Octobre 2019',
-		value: '10-2019',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2019-octobre/'
-	},
-	{
-		label: 'Juin 2019',
-		value: '06-2019',
-		href: 'https://observatoire.numerique.gouv.fr/observatoire/2019-juin/'
-	}
-];
-
 export function Top250Header(props: Props) {
 	const { title, searchLabel, onSearch } = props;
+
+	const { data: editions } = useEditions();
+
+	const editionOptions = [
+		{
+			label: editions && editions[0] ? editions[0].name : '...',
+			value: '04-2023'
+		},
+		{
+			label: 'Octobre 2022',
+			value: '10-2022',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-octobre/'
+		},
+		{
+			label: 'Juillet 2022',
+			value: '07-2022',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-juillet/'
+		},
+		{
+			label: 'Avril 2022',
+			value: '04-2022',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-avril/'
+		},
+		{
+			label: 'Janvier 2022',
+			value: '01-2022',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2022-janvier/'
+		},
+		{
+			label: 'Octobre 2021',
+			value: '10-2021',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-octobre/'
+		},
+		{
+			label: 'Juillet 2021',
+			value: '07-2021',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-juillet/'
+		},
+		{
+			label: 'Avril 2021',
+			value: '04-2021',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-avril/'
+		},
+		{
+			label: 'Janvier 2021',
+			value: '01-2021',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2021-janvier/'
+		},
+		{
+			label: 'Octobre 2020',
+			value: '10-2020',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-octobre/'
+		},
+		{
+			label: 'Juillet 2020',
+			value: '07-2020',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-juillet/'
+		},
+		{
+			label: 'Avril 2020',
+			value: '04-2020',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-avril/'
+		},
+		{
+			label: 'Janvier 2020',
+			value: '01-2020',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2020-janvier/'
+		},
+		{
+			label: 'Octobre 2019',
+			value: '10-2019',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2019-octobre/'
+		},
+		{
+			label: 'Juin 2019',
+			value: '06-2019',
+			href: 'https://observatoire.numerique.gouv.fr/observatoire/2019-juin/'
+		}
+	];
 
 	const { classes, cx } = useStyles();
 
