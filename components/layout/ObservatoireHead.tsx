57ec100a54640be35aad177e883896eee9a75b2f
Clément <clement@numericite.eu>
2023-06-22 17:28:14 +0200

    fix: meta

--- components/layout/ObservatoireHead.tsx
+++ components/layout/ObservatoireHead.tsx
@@ -4,43 +4,39 @@ import { useRouter } from 'next/router';
 export function ObservatoireHead() {
 	const router = useRouter();
 
-	let pageTitlePrefix = '';
+	let pageTitle =
+		'Accueil - Observatoire - Vos démarches numériques essentielles';
 	let og = {
 		title: `Vos démarches numériques essentielles`,
 		description: `Ce dispositif recense les démarches et services numériques
 		essentiels, et permet d’analyser, chaque trimestre, leur qualité à
 		travers des indicateurs clés.`,
-		imageUrl: 'https://observatoire.numerique.gouv.fr/assets/observatoire.png'
+		imageUrl: 'https://observatoire.numerique.gouv.fr/assets/observatoire.png',
+		url: 'https://observatoire.numerique.gouv.fr'
 	};
 
 	switch (router.pathname) {
-		case '/':
-			pageTitlePrefix = 'Accueil -';
-			break;
 		case '/observatoire':
-			pageTitlePrefix = 'Observatoire -';
+			pageTitle = 'Observatoire - Vos démarches numériques essentielles';
 			og.title = `Vos démarches numériques essentielles`;
 			break;
 		case '/Aide/Observatoire':
-			pageTitlePrefix = 'En savoir plus -';
+			pageTitle = 'En savoir plus - Vos démarches numériques essentielles';
 			og.title = 'En savoir plus';
 			break;
 		case '/demande':
-			pageTitlePrefix = 'Soumettre une démarche ou un service -';
+			pageTitle = 'Soumettre une démarche ou un service -';
 			break;
 	}
 
 	return (
 		<Head>
-			<title>{pageTitlePrefix} Vos démarches numériques essentielles</title>
+			<title>{pageTitle}</title>
 			<meta name="description" content={og.description}></meta>
-			<meta property="og:url" content={og.imageUrl}></meta>
+			<meta property="og:url" content={og.url}></meta>
 			<meta property="og:title" content={og.title}></meta>
 			<meta property="og:description" content={og.description}></meta>
-			<meta
-				property="og:image"
-				content="https://observatoire.numerique.gouv.fr/assets/observatoire.png"
-			></meta>
+			<meta property="og:image" content={og.imageUrl}></meta>
 		</Head>
 	);
 }
