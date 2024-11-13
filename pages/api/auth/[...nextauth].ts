a4f38261b1745be7f7e7076da91621f48435aeef
Clément <clement@numericite.eu>
2024-11-13 17:26:17 +0100

    fix: cookie custom token next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -63,7 +63,19 @@ const options = {
 		verificationKey: process.env.JWT_VERIFICATION_PUBLIC_KEY,
 		encryptionKey: process.env.JWT_ENCRYPTION_PRIVATE_KEY,
 		decryptionKey: process.env.JWT_DECRYPTION_PUBLIC_KEY
-	}
+	},
+
+	cookies: {
+		sessionToken: {
+			name: 'observatoire_auth_token',
+			options: {
+				httpOnly: true,
+				sameSite: 'lax',
+				path: '/',
+				secure: true
+			}
+		}
+	},
 };
 
 const authHandler: NextApiHandler = (
