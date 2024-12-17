080ba6317a4f17a48f987234612512b56829b3c6
Clément <clement@numericite.eu>
2024-12-17 10:22:53 +0100

    fix: wording indicator levels

--- src/components/top250/table/IndicatorContent.tsx
+++ src/components/top250/table/IndicatorContent.tsx
@@ -36,7 +36,7 @@ export const IndicatorContent = (props: Props) => {
 				</div>
 			)}
 			<p>
-				<b>{indicator.levels?.docs?.length} niveaux d'évaluation</b>
+				<b>Status</b>
 			</p>
 			<ul className={cx(classes.indicatorContainer)}>
 				{indicator.levels?.docs &&
