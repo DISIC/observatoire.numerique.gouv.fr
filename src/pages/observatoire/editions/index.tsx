cdfa4c9c3dd6a3e72990cd8be9dea3e49e102b66
Antoine Lelong <azenoxe@gmail.com>
2025-02-27 14:50:17 +0100

    fix: add old editions and add query parameter for tabId in page of editions list

--- src/pages/observatoire/editions/index.tsx
+++ src/pages/observatoire/editions/index.tsx
@@ -2,10 +2,18 @@ import { useEditions } from '@/utils/api';
 import { fr } from '@codegouvfr/react-dsfr';
 import Tabs from '@codegouvfr/react-dsfr/Tabs';
 import { Edition } from '@prisma/client';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { tss } from 'tss-react';
 import { formatDateRangeFR } from '@/utils/tools';
 import Button from '@codegouvfr/react-dsfr/Button';
+import { useRouter } from 'next/router';
+
+type EditionTab = {
+	kind: 'base' | 'old';
+	id: string;
+	name: string;
+	period: string;
+};
 
 const oldEditions = [
 	{ slug: '2022-octobre', display: 'Octobre 2022' },
@@ -27,46 +35,84 @@ const oldEditions = [
 export default function ObservatoireEditions() {
 	const { classes, cx } = useStyles();
 
-	const [selectedTabId, setSelectedTabId] = useState<string>('2024');
+	const router = useRouter();
+
+	const [selectedTabId, setSelectedTabId] = useState<string>('tab1');
 
 	let { data: editions } = useEditions();
 
 	editions = editions?.slice(1) || [];
 
-	const editionTabs = editions.reduce((acc, edition) => {
-		const editionYearFromName = edition.name.split(' ')[1];
-		const year = editionYearFromName
-			? editionYearFromName
-			: new Date(edition.end_date).getFullYear().toString();
+	const handleTabChange = (tabId: string) => {
+		router.push(`?tab=${tabId}`, undefined, { shallow: true });
+	};
 
-		return acc.some(item => item.label === year)
-			? acc.map(item =>
-					item.label === year
-						? {
-								...item,
-								editions: [...item.editions, edition]
-						  }
-						: item
-			  )
-			: [...acc, { label: year, tabId: year, editions: [edition] }];
-	}, [] as { label: string; tabId: string; editions: Edition[] }[]);
+	const editionTabs = useMemo(() => {
+		const groupedByYear: Record<string, EditionTab[]> = {};
 
-	const tableHeader = ['Nom', 'Début', 'Tableau de suivi'];
+		editions.forEach(edition => {
+			const editionYearFromName = edition.name.split(' ')[1];
+			const year =
+				editionYearFromName ||
+				new Date(edition.end_date).getFullYear().toString();
+
+			if (!groupedByYear[year]) groupedByYear[year] = [];
+
+			groupedByYear[year].push({
+				kind: 'base',
+				id: edition.id,
+				name: edition.name,
+				period: formatDateRangeFR(
+					new Date(edition.start_date),
+					new Date(edition.end_date)
+				)
+			});
+		});
+
+		oldEditions.forEach(edition => {
+			const year = edition.display.split(' ')[1];
+
+			if (!groupedByYear[year]) groupedByYear[year] = [];
+
+			groupedByYear[year].push({
+				kind: 'old',
+				id: edition.slug,
+				name: edition.display,
+				period: ''
+			});
+		});
+
+		return Object.entries(groupedByYear)
+			.sort(([yearA], [yearB]) => parseInt(yearB) - parseInt(yearA))
+			.map(([year, editions], index) => ({
+				label: year,
+				tabId: `tab${index}`,
+				editions: editions as EditionTab[]
+			}));
+	}, [editions]);
+
+	const tableHeader = ['Éditions', 'Périodes', 'Tableau de suivi'];
 
 	const currentTableData = useMemo(() => {
 		const currentEdition = editionTabs.find(
-			edition => edition.tabId === selectedTabId
+			edition => edition.tabId == selectedTabId
 		);
 		if (!currentEdition) return [];
 		return currentEdition.editions.map(edition => ({
 			name: edition.name,
-			period: formatDateRangeFR(
-				new Date(edition.start_date),
-				new Date(edition.end_date)
-			),
-			link: `/observatoire/editions/${edition.id}`
+			period: edition.period,
+			link: `/observatoire/${edition.kind === 'base' ? 'editions' : 'old'}/${
+				edition.id
+			}`
 		})) as { name: string; period: string; link: string }[];
 	}, [selectedTabId, editionTabs]);
+
+	useEffect(() => {
+		const { tab } = router.query;
+		if (tab && editions.length > 0) {
+			setSelectedTabId(tab as string);
+		}
+	}, [router.isReady, router.query, editions]);
 
 	return (
 		<>
@@ -76,9 +122,9 @@ export default function ObservatoireEditions() {
 			<div className={cx(classes.tableContainer)}>
 				<div className="fr-container fr-py-6v">
 					<Tabs
-						selectedTabId={selectedTabId}
 						tabs={editionTabs}
-						onTabChange={setSelectedTabId}
+						selectedTabId={selectedTabId}
+						onTabChange={(e: string) => handleTabChange(e)}
 						className={cx(classes.tabs)}
 					>
 						<div className={cx(fr.cx('fr-table'), classes.table)}>
@@ -132,9 +178,21 @@ const useStyles = tss.withName(ObservatoireEditions.name).create(() => ({
 		backgroundColor: fr.colors.decisions.background.contrast.info.default
 	},
 	table: {
+		margin: 0,
+		marginTop: fr.spacing('2v'),
+		['table']: {
+			display: 'inline-table'
+		},
 		['table thead']: {
+			backgroundColor: 'white',
 			backgroundImage: `linear-gradient(0deg, ${fr.colors.decisions.artwork.major.blueFrance.default}, ${fr.colors.decisions.artwork.major.blueFrance.default})`
 		}
 	},
-	tabs: {}
+	tabs: {
+		boxShadow: 'none',
+		height: 'auto',
+		['.fr-tabs__panel']: {
+			padding: 0
+		}
+	}
 }));
