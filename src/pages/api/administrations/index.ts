0d3d113307d2be5580dcdb0f31713ce3c65f8e80
Clément <clement@numericite.eu>
2025-03-05 13:50:39 +0100

    fix: remove legacy auth check

--- src/pages/api/administrations/index.ts
+++ src/pages/api/administrations/index.ts
@@ -17,14 +17,6 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	if (['POST', 'PUT', 'DELETE'].includes(req.method || '')) {
-		const jwtCookie =
-			req.cookies[process.env.NEXT_PUBLIC_JWT_COOKIE_NAME ?? 'obs-jwt'];
-		if (!jwtCookie) {
-			return res.status(401).json({ msg: 'You shall not pass.' });
-		}
-	}
-
 	if (req.method === 'GET') {
 		const administrations = await getAdministrations();
 		res.status(200).json(administrations);
