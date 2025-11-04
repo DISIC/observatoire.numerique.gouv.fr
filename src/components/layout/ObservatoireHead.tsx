be12032300d602c6c11ed37618c4b5696b0cfeb5
Antoine Lelong <azenoxe@gmail.com>
2025-11-04 10:46:01 +0100

    fix: add missing title to data-viz pages and others

--- src/components/layout/ObservatoireHead.tsx
+++ src/components/layout/ObservatoireHead.tsx
@@ -1,3 +1,5 @@
+import { ProcedureKind } from '@/pages/api/indicator-scores';
+import { base64UrlToString, getProcedureKindLabel } from '@/utils/tools';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
@@ -19,6 +21,17 @@ export function ObservatoireHead() {
 			pageTitle = 'Tableau de suivi - Vos démarches essentielles';
 			og.title = `Vos démarches essentielles`;
 			break;
+		case '/observatoire/editions':
+			pageTitle = 'Éditions - Vos démarches essentielles';
+			og.title = 'Éditions';
+			break;
+		case '/observatoire/editions/[slug]':
+		case '/observatoire/old/[slug]':
+			const { slug: editionSlug } = router.query as { slug: string };
+			const formattedEditionSlug = editionSlug.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+			pageTitle = `Éditions - Tableau de suivi - ${formattedEditionSlug} - Vos démarches essentielles`;
+			og.title = `Éditions - Tableau de suivi - ${formattedEditionSlug}`;
+			break;
 		case '/Aide/Observatoire':
 			pageTitle =
 				'Méthodologie et calcul des indicateurs - Vos démarches essentielles';
@@ -26,6 +39,18 @@ export function ObservatoireHead() {
 			break;
 		case '/demande':
 			pageTitle = 'Soumettre une démarche - Vos démarches essentielles';
+			break;
+		case '/data-viz/[kind]':
+		case '/data-viz/procedure':
+			const { kind } = router.query;
+			const slugTitle = getProcedureKindLabel(kind as ProcedureKind, { plural: true, uppercaseFirst: true}) || 'Démarches';
+			pageTitle = `Graphiques - ${slugTitle} - Vos démarches essentielles`;
+			break;
+		case '/data-viz/[kind]/[slug]':
+			const { kind: kindDetail, slug } = router.query;
+			const procedureTitle = slug ? base64UrlToString(slug as string) : 'Démarche';
+			const kindLabel = getProcedureKindLabel(kindDetail as ProcedureKind, { plural: true, uppercaseFirst: true }) || 'Démarche';
+			pageTitle = `Graphiques - ${procedureTitle} (${kindLabel}) - Vos démarches essentielles`;
 			break;
 	}
 
