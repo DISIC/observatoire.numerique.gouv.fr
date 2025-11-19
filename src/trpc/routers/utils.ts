352d6ff06a924c665eb8f8bb6bed47a6c14524bc
Clément <clement@numericite.eu>
2025-11-19 13:51:14 +0100

    fix: ref to administration centrale

--- src/trpc/routers/utils.ts
+++ src/trpc/routers/utils.ts
@@ -1,6 +1,9 @@
-import { ProcedureWithFields } from "@/pages/api/procedures/types";
-import { PayloadIndicator, PayloadIndicatorLevel } from "@/payload/payload-types";
-import { IndicatorColor } from "@prisma/client";
+import { ProcedureWithFields } from '@/pages/api/procedures/types';
+import {
+	PayloadIndicator,
+	PayloadIndicatorLevel
+} from '@/payload/payload-types';
+import { IndicatorColor } from '@prisma/client';
 
 export const grist_field_names = {
 	edition: 'Ref_Edition',
@@ -9,7 +12,7 @@ export const grist_field_names = {
 	link: 'URL_Demarche',
 	title: 'Nom_Demarche',
 	administration: 'Nom_Administration',
-	administration_central: 'Ref_AC_SG',
+	administration_central: 'Nom_Administration_Centrale',
 	sousorg: 'Nom_Administration_Centrale',
 	ministere: 'Nom_Ministere',
 	volume: 'Volumetrie_En_Ligne',
@@ -36,7 +39,7 @@ const grist_field_names_percentages = [
 	grist_field_names.indicators.uptime,
 	grist_field_names.indicators.help_used,
 	grist_field_names.indicators.help_reachable
-]
+];
 
 const grist_field_names_strings = [
 	'usage',
@@ -44,7 +47,7 @@ const grist_field_names_strings = [
 	'uptime',
 	'help_used',
 	'help_reachable'
-]
+];
 
 export type GristFields = typeof grist_field_names;
 
@@ -52,70 +55,87 @@ export const getFieldsFromGristProcedure = (
 	gristProcedure: any,
 	indicators: PayloadIndicator[]
 ): ProcedureWithFields['fields'] => {
-	return indicators.map(indicator => {
-		const indicatorLevels = ((indicator.levels?.docs || []) as PayloadIndicatorLevel[]);
-		const gristColumnName = grist_field_names.indicators[indicator.slug as keyof GristFields['indicators']]
-		let value = gristProcedure[gristColumnName]
+	return indicators
+		.map(indicator => {
+			const indicatorLevels = (indicator.levels?.docs ||
+				[]) as PayloadIndicatorLevel[];
+			const gristColumnName =
+				grist_field_names.indicators[
+					indicator.slug as keyof GristFields['indicators']
+				];
+			let value = gristProcedure[gristColumnName];
 
-		if (value === null) {
-			return {
-				id: `preview-${indicator.id}`,
-				slug: indicator.slug,
-				label: '-',
-				value: null,
-				color: 'gray' as IndicatorColor,
-				noBackground: true,
-				procedureId: 'preview',
-				goalReached: false
-			}
-		}
-
-		const isPercentageCol = grist_field_names_percentages.includes(gristColumnName)
-		if (isPercentageCol && !isNaN(value)) {
-			value = (value * 100).toFixed(1).replace(/\.0$/, '');
-		}
-
-		if (!isNaN(value)) {
-			const indicatorLevel = indicatorLevels.filter(
-				(level) => level.threshold !== undefined && level.threshold !== null
-			).sort(
-				(a, b) => (b.threshold ?? 10000) - (a.threshold ?? 10000)
-			).find(
-				(level) => (level.threshold ?? 10000) <= value
-			);
-
-			if (indicatorLevel) {
+			if (value === null) {
 				return {
 					id: `preview-${indicator.id}`,
 					slug: indicator.slug,
-					label: indicatorLevel.label.replace(/X{1,5}/g, isPercentageCol ? value.toString() : (Math.round(value * 10) / 10).toFixed(1)),
-					value: value !== null ? value.toString() : value,
+					label: '-',
+					value: null,
+					color: 'gray' as IndicatorColor,
+					noBackground: true,
+					procedureId: 'preview',
+					goalReached: false
+				};
+			}
+
+			const isPercentageCol =
+				grist_field_names_percentages.includes(gristColumnName);
+			if (isPercentageCol && !isNaN(value)) {
+				value = (value * 100).toFixed(1).replace(/\.0$/, '');
+			}
+
+			if (!isNaN(value)) {
+				const indicatorLevel = indicatorLevels
+					.filter(
+						level => level.threshold !== undefined && level.threshold !== null
+					)
+					.sort((a, b) => (b.threshold ?? 10000) - (a.threshold ?? 10000))
+					.find(level => (level.threshold ?? 10000) <= value);
+
+				if (indicatorLevel) {
+					return {
+						id: `preview-${indicator.id}`,
+						slug: indicator.slug,
+						label: indicatorLevel.label.replace(
+							/X{1,5}/g,
+							isPercentageCol
+								? value.toString()
+								: (Math.round(value * 10) / 10).toFixed(1)
+						),
+						value: value !== null ? value.toString() : value,
+						color: indicatorLevel.color,
+						noBackground: indicatorLevel.noBackground || false,
+						procedureId: 'preview',
+						goalReached: indicatorLevel.goal_reached || false
+					};
+				}
+			}
+
+			const indicatorLevel = indicatorLevels.find(
+				(level: PayloadIndicatorLevel) => level.label === value
+			);
+			if (indicatorLevel && typeof indicatorLevel !== 'string') {
+				return {
+					id: `preview-${indicator.id}`,
+					slug: indicator.slug,
+					label: value,
+					value:
+						indicator.slug === 'online'
+							? gristProcedure[grist_field_names.link]
+							: value
+							? value.toString()
+							: value,
 					color: indicatorLevel.color,
 					noBackground: indicatorLevel.noBackground || false,
 					procedureId: 'preview',
 					goalReached: indicatorLevel.goal_reached || false
-				}
+				};
 			}
-		}
 
-		const indicatorLevel = indicatorLevels.find((level: PayloadIndicatorLevel) => level.label === value);
-		if (indicatorLevel && typeof indicatorLevel !== 'string') {
-			return {
-				id: `preview-${indicator.id}`,
-				slug: indicator.slug,
-				label: value,
-				value: indicator.slug === 'online' ? gristProcedure[grist_field_names.link] : value ? value.toString() : value,
-				color: indicatorLevel.color,
-				noBackground: indicatorLevel.noBackground || false,
-				procedureId: 'preview',
-				goalReached: indicatorLevel.goal_reached || false
-			}
-		}
-
-		return null;
-
-	}).filter((field) => field !== null);
-}
+			return null;
+		})
+		.filter(field => field !== null);
+};
 
 export const getFieldsFromProcedure = (
 	procedure: Pick<ProcedureWithFields, 'id' | 'fields'>,
@@ -135,7 +155,11 @@ export const getFieldsFromProcedure = (
 				(level: PayloadIndicatorLevel) => level.label === currentField.label
 			);
 
-			let value = currentField.value ? currentField.value : indicatorLevel ? currentField.label : null;
+			let value = currentField.value
+				? currentField.value
+				: indicatorLevel
+				? currentField.label
+				: null;
 
 			if (value === null) {
 				return {
