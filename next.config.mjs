c31d59b4f0dab50ca01c8b521cf00ed5116014ea
Clément Lelong <clement@numericite.eu>
2026-01-28 11:17:49 +0100

    fix: avoid build issues

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,20 @@ import { withPayload } from '@payloadcms/next/withPayload';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true,
 	images: {
 		minimumCacheTTL: 604800
-	},
-	experimental: {
-		esmExternals: true
 	},
 	i18n: {
 		locales: ['fr'],
 		defaultLocale: 'fr'
 	},
-	webpack: config => {
+	webpack: (config, { isServer }) => {
 		config.module.rules.push({
 			test: /\.woff2$/,
 			type: 'asset/resource'
 		});
+
 		return config;
-	},
-	babel: {
-		presets: [
-			['@babel/preset-env', { targets: { node: 'current' } }],
-			'@babel/preset-typescript',
-			'next/babel'
-		]
 	},
 	transpilePackages: ['@codegouvfr/react-dsfr', 'tss-react']
 };
